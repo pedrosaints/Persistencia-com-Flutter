24a8641faa884baa276d022e2be35e45d9c52c16
Pedro Henrique S B <pedrosaints@alu.ufc.br>
2020-09-24 14:21:15 -0300

    Criando repositorio curso 2

---
+++ android/app/src/main/kotlin/com/example/bytebank_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bytebank_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Dashboard'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Image.asset('images/bytebank_logo.png'),
+            Container(
+              height: 120,
+              width: 100,
+              color: Colors.green,
+              child: Column(
+                children: <Widget>[
+                  Icon(Icons.people),
+                  Text('Contacts'),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
